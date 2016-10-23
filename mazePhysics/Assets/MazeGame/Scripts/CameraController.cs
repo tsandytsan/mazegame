6ede6aa9e6487e47784a94234f11fc3bd49a7498
ssoto42046 <sigifredo.soto@gmail.com>
2016-10-22 18:02:50 -0700

    pulling

--- mazePhysics/Assets/MazeGame/Scripts/CameraController.cs
+++ mazePhysics/Assets/MazeGame/Scripts/CameraController.cs
@@ -8,10 +8,10 @@ public class CameraController : MonoBehaviour {
 	void Start () {
         offset = transform.position - player.transform.position;
 	}
-	
-	// Update is called once per frame
-	void LateUpdate () {
+
+    // Update is called once per frame
+    void LateUpdate() {
         transform.position = player.transform.position + offset;
- 
+        transform.Rotate(Vector3, 45.0)
+           }
 	}
-}
