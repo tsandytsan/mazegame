912b451ce36ddace6a02c65023bd3b5a61ad8033
ssoto42046 <sigifredo.soto@gmail.com>
2016-10-22 20:03:24 -0700

    Added fixed camera control system
    Copied script basically

--- mazePhysics/Assets/MazeGame/Scripts/CameraController.cs
+++ mazePhysics/Assets/MazeGame/Scripts/CameraController.cs
@@ -2,16 +2,39 @@
 using System.Collections;
 
 public class CameraController : MonoBehaviour {
-    public GameObject player;
-    private Vector3 offset;
-	// Use this for initialization
-	void Start () {
-        offset = transform.position - player.transform.position;
-	}
+    public Transform lookAt;
+    public Transform camTransform;
+    // Use this for initializatio
+    private Camera cam;
+    private float distance = 10.0f;
+    private float currentX = 0.0f;
+    private float currentY = 0.0f;
 
-    // Update is called once per frame
-    void LateUpdate() {
-        transform.position = player.transform.position + offset;
-        transform.Rotate(Vector3, 45.0)
-           }
+    private float sensitivityX = 4.0f;
+    private float sensitivityY = 1.0f;
+
+    private const float Y_ANGLE_MIN = 0.0f;
+    private const float Y_ANGLE_MAX = 50.0f;
+
+
+
+	void Start () {
+        camTransform = transform;
+        cam = Camera.main;
+	}
+    private void Update()
+    {
+        currentX += Input.GetAxis("Mouse X");
+        currentY += Input.GetAxis("Mouse Y");
+
+        //currentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+    }
+    private void LateUpdate()
+    {
+        Vector3 dir = new Vector3(0, 0, -distance);
+        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+        camTransform.position = lookAt.position + rotation * dir;
+        camTransform.LookAt(lookAt.position);
+    }
+
 	}
